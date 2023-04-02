ebe73948e787d75cb8a47096a48302500a295b52
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-04-01 20:43:50 -0500

    changed FK to stone table

---
+++ Migrations/20230402013320_ChangedFKToStone.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tsx_react_project.Migrations
+{
+    public partial class ChangedFKToStone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JewelryPieces_Cabs_stoneId",
+                table: "JewelryPieces");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JewelryPieces_stoneId",
+                table: "JewelryPieces");
+
+            migrationBuilder.DropColumn(
+                name: "stoneId",
+                table: "JewelryPieces");
+
+            migrationBuilder.AddColumn<int>(
+                name: "jewelryId",
+                table: "Cabs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cabs_jewelryId",
+                table: "Cabs",
+                column: "jewelryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cabs_JewelryPieces_jewelryId",
+                table: "Cabs",
+                column: "jewelryId",
+                principalTable: "JewelryPieces",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cabs_JewelryPieces_jewelryId",
+                table: "Cabs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cabs_jewelryId",
+                table: "Cabs");
+
+            migrationBuilder.DropColumn(
+                name: "jewelryId",
+                table: "Cabs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "stoneId",
+                table: "JewelryPieces",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JewelryPieces_stoneId",
+                table: "JewelryPieces",
+                column: "stoneId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JewelryPieces_Cabs_stoneId",
+                table: "JewelryPieces",
+                column: "stoneId",
+                principalTable: "Cabs",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
